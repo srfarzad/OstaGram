2ffdc63fdc5b5fc805a7d295edff95a5555befc7
farzad <sr.farzad@gmail.com>
2018-12-13 17:45:47 +0330

    Session 2
    complete retrofit and post
    
    viewpager indicator
    
    make library

--- app/src/main/java/com/ostagram/models/IMessageListener.java
+++ app/src/main/java/com/ostagram/models/IMessageListener.java
@@ -5,6 +5,8 @@ import java.util.List;
 public interface IMessageListener<T> {
 
 
+    public void onSuccessObject(T response);
+
     public void onSuccess(List<T> response);
 
     public void onError(String errorMessage);
--- app/src/main/java/com/ostagram/webservice/ApiInterface.java
+++ app/src/main/java/com/ostagram/webservice/ApiInterface.java
@@ -4,6 +4,7 @@ import com.ostagram.models.Posts;
 
 import java.util.List;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
@@ -17,5 +18,28 @@ public interface ApiInterface {
     @POST("myPosts.php")
     @FormUrlEncoded
     Call<List<Posts>>  getAllPosts(@Field("from") int from , @Field("to") int to );
+
+
+    //------
+
+    @POST("user_information.php")
+    @FormUrlEncoded
+    Call<ResponseBody>  getInformation(@Field("id") String userId);
+
+
+
+    @POST("login.php")
+    @FormUrlEncoded
+    Call<ResponseBody>  login(@Field("username") int username , @Field("password") int password );
+
+
+
+    @POST("register.php")
+    @FormUrlEncoded
+    Call<ResponseBody>  register(@Field("username") int username , @Field("password") int password );
+
+
+
+
 
 }
